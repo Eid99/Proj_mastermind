da88553f50d84e9348d935100a7ca543e47de4c2
PedroEid <38227797+PedroEid@users.noreply.github.com>
2019-03-20 18:19:58 +0000

    numero de cores

--- prj.cpp
+++ prj.cpp
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void criador_chave(int chave[]){
-  int n_chave;
-  n_chave=rand()%12-6;
+void criador_chave(int chave[],int numero_cores,int tamanho_chave){
+  for(int i=0;i<tamanho_chave;i++)
+  {
+    chave[i]=rand()%numero_cores;
+  }
 }
 
 int main(){
 
-  int numero_jogadores=0,tamanho_chave=1;
+  int numero_jogadores=0,tamanho_chave=1,numero_cores=0;
 
 while(numero_jogadores<1 || numero_jogadores>4){
   puts("Quantos Jogadores de 1 a 4:");
   scanf("%d",&numero_jogadores);}
+while(numero_cores<6 || numero_cores>12){
+puts("\nNumero de cores de 6 a 12:");
+scanf("%d",&numero_cores);
+}
 while(tamanho_chave<4 || tamanho_chave>8){
   fflush(stdin);
-  puts("Tamanho da chave de 4 a 8:");
+  puts("\nTamanho da chave de 4 a 8:");
   scanf("%d",&tamanho_chave);
 }
 
@@ -24,10 +30,8 @@ while(tamanho_chave<4 || tamanho_chave>8){
   for(int i=0;i<numero_jogadores;i++)
   {
 
-
-
-    puts("Nome dos jogadores");
-    scanf(" %s", &Nome_jogadores[i]);
+    puts("\nNome dos jogadores");
+    scanf(" %s", Nome_jogadores[i]);
 }
 printf("%s", Nome_jogadores[1]);
   return 0;}
