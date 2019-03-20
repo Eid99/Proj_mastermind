2a7b839c4ee8088bf070759f36dba191ff28cdca
PedroEid <38227797+PedroEid@users.noreply.github.com>
2019-03-20 16:35:17 +0000

    consertei os nomes

--- prj.cpp
+++ prj.cpp
@@ -1,3 +1,17 @@
 #include <stdio.h>
-main(){
-  printf("Hello world");}
+int main(){
+  int numero_jogadores;
+  puts("Quantos Jogadores");
+  scanf("%d",&numero_jogadores);
+char Nome_jogadores[numero_jogadores][20];
+
+  for(int i=0;i<numero_jogadores;i++)
+  {
+
+
+
+    puts("Nome dos Nome jogadores");
+    scanf(" %s", Nome_jogadores[i]);
+}
+printf("%s", Nome_jogadores[1]);
+  return 0;}
