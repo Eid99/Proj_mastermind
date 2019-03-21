b92765c51f442b372f4b46e1c6ae0d914618fd00
PedroEid <38227797+PedroEid@users.noreply.github.com>
2019-03-21 16:04:52 +0000

    melhora do sistema de criacao de chaves

--- prj.cpp
+++ prj.cpp
@@ -1,37 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
 void criador_chave(int chave[],int numero_cores,int tamanho_chave){
+  int cor;
   for(int i=0;i<tamanho_chave;i++)
   {
-    chave[i]=rand()%numero_cores;
+    cor=rand()%numero_cores+1;
+    switch (cor) {
+      case 1: chave[i]='A'; break;
+      case 2: chave[i]='B'; break;
+      case 3: chave[i]='C'; break;
+      case 4: chave[i]='D'; break;
+      case 5: chave[i]='E'; break;
+      case 6: chave[i]='F'; break;
+      case 7: chave[i]='G'; break;
+      case 8: chave[i]='H'; break;
+      case 9: chave[i]='I'; break;
+      case 10: chave[i]='J'; break;
+      case 11: chave[i]='K'; break;
+      case 12: chave[i]='L'; break;
+
+
+    }
   }
 }
 
+
+
+
 int main(){
+// INICIALIZACAO
 
   int numero_jogadores=0,tamanho_chave=1,numero_cores=0;
 
 while(numero_jogadores<1 || numero_jogadores>4){
+  fflush(stdin);
   puts("Quantos Jogadores de 1 a 4:");
   scanf("%d",&numero_jogadores);}
 while(numero_cores<6 || numero_cores>12){
-puts("\nNumero de cores de 6 a 12:");
-scanf("%d",&numero_cores);
+  fflush(stdin);
+  puts("\nNumero de cores de 6 a 12:");
+  scanf(" %d",&numero_cores);
 }
 while(tamanho_chave<4 || tamanho_chave>8){
   fflush(stdin);
   puts("\nTamanho da chave de 4 a 8:");
-  scanf("%d",&tamanho_chave);
+  scanf(" %d",&tamanho_chave);
 }
+
+
 
   int chave[tamanho_chave];
   char Nome_jogadores[numero_jogadores][20];
 
   for(int i=0;i<numero_jogadores;i++)
   {
-
+    fflush(stdin);
     puts("\nNome dos jogadores");
     scanf(" %s", Nome_jogadores[i]);
 }
-printf("%s", Nome_jogadores[1]);
+
+criador_chave(chave, numero_cores,tamanho_chave);
+for(int i=0;i<tamanho_chave;i++){
+  printf("%c\n",chave[i]);
+}
+
+//FIM DA INICIALIZACAO
+
+
+
   return 0;}
