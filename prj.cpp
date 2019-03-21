b1a31c67e0aa741bf9a794bd6e171a7560c7b3ca
PedroEid <38227797+PedroEid@users.noreply.github.com>
2019-03-21 20:44:44 +0000

    Controle de sistema de repeticao da chave senha terminado

--- prj.cpp
+++ prj.cpp
@@ -1,36 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctime>
+using namespace std;
 
 
-void criador_chave(int chave[],int numero_cores,int tamanho_chave){
-  int cor;
+void criador_chave(int chave[],int numero_cores,int tamanho_chave, char repetir_cores){
+  int cor,repeticao[numero_cores]={0},voltar_rand=0,j=0;
+  srand ( time(NULL) );
   for(int i=0;i<tamanho_chave;i++)
   {
-    cor=rand()%numero_cores+1;
-    switch (cor) {
-      case 1: chave[i]='A'; break;
-      case 2: chave[i]='B'; break;
-      case 3: chave[i]='C'; break;
-      case 4: chave[i]='D'; break;
-      case 5: chave[i]='E'; break;
-      case 6: chave[i]='F'; break;
-      case 7: chave[i]='G'; break;
-      case 8: chave[i]='H'; break;
-      case 9: chave[i]='I'; break;
-      case 10: chave[i]='J'; break;
-      case 11: chave[i]='K'; break;
-      case 12: chave[i]='L'; break;
+    voltar_rand=0;
+    do{
+
+
+      cor=rand()%numero_cores+1;
+      for(j=0; j<numero_cores  && (repetir_cores=='N'|| repetir_cores=='n');j++){
+        if (repeticao[j]==cor){
+          voltar_rand=1;
+          break;
+          }
+          else{
+          voltar_rand=0;
+
+        }
+      }
+
+
+    }while(voltar_rand==1);
+
+      switch (cor) {
+        case 1: chave[i]='A'; break;
+        case 2: chave[i]='B'; break;
+        case 3: chave[i]='C'; break;
+        case 4: chave[i]='D'; break;
+        case 5: chave[i]='E'; break;
+        case 6: chave[i]='F'; break;
+        case 7: chave[i]='G'; break;
+        case 8: chave[i]='H'; break;
+        case 9: chave[i]='I'; break;
+        case 10: chave[i]='J'; break;
+        case 11: chave[i]='K'; break;
+        case 12: chave[i]='L'; break;
+      }repeticao[i]=cor;
 
 
     }
-  }
+
+
 }
+
 
 
 
 
 int main(){
 // INICIALIZACAO
+srand ( time(NULL) );
 
   int numero_jogadores=0,tamanho_chave=1,numero_cores=0;
 
@@ -53,7 +78,13 @@ while(tamanho_chave<4 || tamanho_chave>8){
 
   int chave[tamanho_chave];
   char Nome_jogadores[numero_jogadores][20];
+  char repetir_cores='e';
 
+
+  while(repetir_cores!='S' && repetir_cores!='N' && repetir_cores!='s' && repetir_cores!='n'){
+    puts("Sera permitido repetir cores: S/N");
+    scanf(" %c",&repetir_cores);
+  }
   for(int i=0;i<numero_jogadores;i++)
   {
     fflush(stdin);
@@ -61,11 +92,10 @@ while(tamanho_chave<4 || tamanho_chave>8){
     scanf(" %s", Nome_jogadores[i]);
 }
 
-criador_chave(chave, numero_cores,tamanho_chave);
+criador_chave(chave, numero_cores,tamanho_chave,repetir_cores);
 for(int i=0;i<tamanho_chave;i++){
   printf("%c\n",chave[i]);
 }
-
 //FIM DA INICIALIZACAO
 
 
